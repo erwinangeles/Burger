ad35efdd43622765f1f5e450025fdf4394768f81
Erwin Angeles <eangeles@hayhouse.com>
2019-11-07 16:41:45 -0600

    added jaws db

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,3 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Double Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Honey BBQ Chicken Strip Sandwich');
+INSERT INTO burgers (burger_name) VALUES ('Jalapeno Bacon Burger');
---
+++ models/burger.js
@@ -0,0 +1,23 @@
+// Import the ORM to create functions that will interact with the database.
+const orm = require('../config/orm.js');
+
+var burger = {
+    selectAll: function(cb) {
+      orm.selectAll("burgers", function(res) {
+        cb(res);
+      });
+    },
+    // The variables cols and vals are arrays.
+    createOne: function(cols, vals, cb) {
+      orm.createOne("burgers", cols, vals, function(res) {
+        cb(res);
+      });
+    },
+    updateOne: function(objColVals, condition, cb) {
+      orm.updateOne("burgers", objColVals, condition, function(res) {
+        cb(res);
+      });
+    }
+  };
+  
+  module.exports = burger;
