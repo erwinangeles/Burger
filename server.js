9f43d747e2f90cd6552bb66422b7851c24defb56
Erwin Angeles <erwin@eang.media>
2019-11-20 09:39:46 -0600

    heroku fix

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 let express = require('express');
 
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(__dirname + '/public'));
+app.use(express.static("public"));
 
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
